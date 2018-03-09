d0fc767c89bfdbb2d55cfc8f629352ab622d9886
spsiddarthan <siddarthan@outlook.com>
2018-03-09 19:18:50 +0530

    Install rails and start the app

--- instances/www.tf
+++ instances/www.tf
@@ -1,10 +1,11 @@
-resource "aws_instance" "www-beta" {
+resource "aws_instance" "www" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "${var.instance_type}"
   tags = {
-    Name = "www-beta"
+    Name = "www"
   }
   associate_public_ip_address = true
   key_name = "${var.key_name}"
   vpc_security_group_ids = ["${var.security_group_id}"]
+  user_data = "${file("${path.module}/install-rails-and-start-app.sh")}"
 }
